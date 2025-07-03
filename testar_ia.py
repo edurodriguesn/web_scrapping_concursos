8320c48fcb24a4f23e7495bc928dc8586936a2ba
Eduardo Rodrigues <edu.rodrigues.ap@gmail.com>
2025-07-03 16:56:43 -0300

    update testar_ia.py

--- testar_ia.py
+++ testar_ia.py
@@ -1,22 +1,26 @@
 from transformers import pipeline
+from deep_translator import GoogleTranslator
 
 classificador_ti = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
 
 labels = [
-            "cargo na área de tecnologia da informação",
-            "cargo em outra área que não é tecnologia",
-        ]
+        "job in the information technology area",
+        "job in an area other than technology",
+    ]
 
 frases = [
-    "tecnico em desenvolvimento educacional especializado (5 vagas)",
+    "técnico especializado em desenvolvimento educacional (5 vagas)",
     "analista judiciário - especialidade: redes de computadores",
     "analista judiciário - especialidade: banco de dados",
-    "auxiliar em desenvolvimento infantil",
-    "tecnico em sistemas para internet"
+    "auxiliar de desenvolvimento infantil",
+    "técnico em desenvolvimento regional"
+    "técnico em sistemas de internet"
 ]
 
 for frase in frases:
-    resultado = classificador_ti(frase, labels)
+    traducao = GoogleTranslator(source='auto', target='en').translate(frase)
+    traducao = traducao.lower().strip()
+    resultado = classificador_ti(traducao, labels)
     if resultado["labels"][0] == labels[0]: #and resultado["scores"][0] > 0.8:
-        print(f"\nFrase: {frase}")
+        print(f"\nFrase: {traducao}")
         print(f"Classificação: {resultado['labels'][0]} - Score: {resultado['scores'][0]:.4f}")
