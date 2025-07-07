9255ec76dda794d372c4f5916d18e5ace30badbc
Eduardo Rodrigues <edu.rodrigues.ap@gmail.com>
2025-07-07 18:57:47 -0300

    refactor: otimizado fluxo principal do sistema

--- src/main.py
+++ src/main.py
@@ -1,22 +1,21 @@
 import scraper.scraper as scraper
 import organizador.organizar_resultado as organizador
-import mensagens.enviar_mensagem as enviar_mensagem
+import src.mensagens.telegram as telegram
 import filtros.filtrar_concursos as filtrar_concursos
 import filtros.prepara_filtro as prepara_filtro
 
 def main():
     print("Buscando concursos...")
-    concursos = scraper.obter_concursos()
-    concursos = prepara_filtro.remover_concursos_expirados_html(concursos)
-    concursos = prepara_filtro.remover_concursos_existentes(concursos)
-    concursos_encontrados = filtrar_concursos.filtrar_texto(concursos)
-    concursos_atuais, concursos_novos = organizador.organizar_dados(concursos_encontrados)
-    
-    # Só envia mensagem se houver concursos novos
-    if concursos_novos:
+    concursos_encontrados = filtrar_concursos.filtrar_texto(
+        prepara_filtro.remover_concursos_existentes(
+            prepara_filtro.remover_concursos_expirados_html(
+                scraper.obter_concursos()
+            )
+        )
+    )
+    if concursos_novos_ti := organizador.organizar_dados(concursos_encontrados)[1]:
         print("Novos concursos encontrados, enviando mensagem...")
-        mensagem_telegram = enviar_mensagem.formatar_mensagem(concursos_novos)
-        enviar_mensagem.enviar_telegram(mensagem_telegram)
+        telegram.enviar_mensagem(concursos_novos_ti)
     else:
         print("Nenhum novo concurso encontrado.")
 
