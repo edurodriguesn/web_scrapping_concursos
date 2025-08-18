515f133c21cf641ef991447cc6ab9c513bb3835b
Eduardo Rodrigues <edu.rodrigues.ap@gmail.com>
2025-08-18 20:46:33 -0300

    refactor(main.py): atualizado fluxo de filtragem de urls obtidas

--- src/main.py
+++ src/main.py
@@ -5,14 +5,15 @@ import filtros.filtrar_concursos as filtrar_concursos
 import filtros.prepara_filtro as prepara_filtro
 
 def main():
-    print("Buscando concursos...")
+    print("Limpando resultados antigos...")
     concursos_encontrados = filtrar_concursos.filtrar_texto(
-        prepara_filtro.remover_concursos_existentes(
-            prepara_filtro.remover_concursos_expirados_html(
+        prepara_filtro.remover_concursos_expirados_html(
+            prepara_filtro.remover_urls_ja_processadas(
                 scraper.obter_concursos()
             )
         )
     )
+    print("Buscando concursos...")
     if concursos_novos_ti := organizador.organizar_dados(concursos_encontrados)[1]:
         print("Novos concursos encontrados, enviando mensagem...")
         telegram.enviar_mensagem(concursos_novos_ti)
